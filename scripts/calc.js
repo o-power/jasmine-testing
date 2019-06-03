7ef30737dbe73f9b9a808816993ba873f5679085
Ollie Power <powerollie@gmail.com>
2019-06-03 15:39:53 +0000

    Added second test and addition function

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,4 +1,8 @@
 // simplest implementation that passes
-function addition() {
-    return 42;
+//function addition() {
+//    return 42;
+//}
+
+function addition(numOne, numTwo) {
+    return numOne + numTwo;
 }
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,8 +1,10 @@
 describe("Calculator", function() {
     describe("Addition tests", function() {
         it("should return 42", function() {
-            expect(addition(20,22)).toBe(42);  
-            //expect(addition(10,9)).toBe(19);
-        })
+            expect(addition(20,22)).toBe(42);
+        });
+        it("should return 19", function() {
+            expect(addition(10,9)).toBe(19);
+        });
     })
 })
